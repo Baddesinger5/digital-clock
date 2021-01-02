60f37391962ff50b89b4e8c53d32ba067da5dcda
baddesinger5 <baddesinger5@gmail.com>
2021-01-02 18:30:04 +0300

    create js code for this clock, 24-format times.

---
+++ main.js
@@ -0,0 +1,22 @@
+
+function clock(){
+    let date = new Date();
+
+    let hours = (date.getHours() < 10) ? '0' + date.getHours() : date.getHours();
+    document.querySelector('#hour').innerHTML = hours;
+
+    let minutes = (date.getMinutes() < 10) ? '0' + date.getMinutes() : date.getMinutes();
+    document.querySelector('#minute').innerHTML = minutes;
+
+    let seconds = (date.getSeconds() < 10) ? '0' + date.getSeconds() : date.getSeconds();
+    document.querySelector('#second').innerHTML = seconds;
+
+    let days = (date.getDate() < 10) ? '0' + date.getDate() : date.getDate();
+    document.querySelector('#number-of-day').innerHTML = days;
+
+    let daysName = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    let nameDate = date.getDay();
+    document.querySelector('#name-date').innerHTML = daysName[nameDate];
+}
+setInterval(clock, 1000);
+clock();
